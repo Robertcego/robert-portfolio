6328673499460b75c6c12585257933158864e6ed
Robertcego <rocerdasgo@gmail.com>
2025-04-14 14:21:58 -0600

    Add changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         components. Proven track record of enhancing user experience, optimizing codebases, and developing custom APIs for global
         companies like 3M and Solventum. Skilled in React.js, JavaScript, and modern frontend frameworks, delivering intuitive and
         responsive interfaces. Adept at collaborating with cross-functional, international teams to drive innovation and streamline
-        development processes. Fluent in English, ensuring seamless communication in global projects
+        development processes. Fluent in English, ensuring seamless communication in global projects.
       </h2>
     </div>
   );
