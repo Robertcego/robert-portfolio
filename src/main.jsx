a3c3284c67a976dcbb76425a4d3c8a6df96e11d2
Robertcego <rocerdasgo@gmail.com>
2025-04-14 14:52:33 -0600

    Update package files

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 
 import { BrowserRouter as Router } from 'react-router-dom';
+
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <Router>
+    <Analytics />
     <App />
   </Router>
 );
