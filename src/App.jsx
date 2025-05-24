a375735f6802d1cba64c7f4b25033f05d5eaf68c
Robert Cerdas <rocerdasgo@gmail.com>
2025-05-23 23:53:40 -0600

    Removed commented code. Added .prettierrc config.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import './App.scss';
 import './global-styles.scss';
-import About from './components/About/About';
-import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,10 @@
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './NavBar.scss';
-import {
-  faGithub,
-  faLinkedin,
-} from '@fortawesome/free-brands-svg-icons';
+
 
 import { faHome } from '@fortawesome/free-solid-svg-icons';
-
-const codeWarsIcon = 'https://raw.githubusercontent.com/codewars/branding/refs/heads/master/logo.png';
 
 
 
@@ -21,38 +16,9 @@ const NavBar = ({ children }) => {
           <NavLink className='home-link' to='/'>
             <FontAwesomeIcon icon={faHome} size='2x' title='Home' />
           </NavLink>
-          {/* <NavLink className='about-link' to='/about' title='About'>
-            <FontAwesomeIcon icon={faUser} size='2x' />
-          </NavLink>
-          <NavLink className='contact-link' to='/contact' title='Contact'>
-            <FontAwesomeIcon icon={faPortrait} size='2x' />
-          </NavLink> */}
         </nav>
       </div>
-      {/* <div className='socials-container'>
-        <ul>
-          <li>
-            <a href='https://github.com/Robertcego' target='_blank' rel='noopener noreferrer'>
-              <FontAwesomeIcon icon={faGithub} size='2x' />
-            </a>
-          </li>
-          <li>
-            <a href='http://linkedin.com/in/robertcego' target='_blank' rel='noopener noreferrer'>
-              <FontAwesomeIcon icon={faLinkedin} size='2x' />
-            </a>
-          </li>
-          <li>
-            <a href='https://www.codewars.com/users/Robertcego' target='_blank' rel='noopener noreferrer'>
-              <img
-                src={codeWarsIcon}
-                alt='Codewars'
-                title='Codewars'
-                style={{ width: '32px', height: '32px' }}
-              />
-            </a>
-          </li>
-        </ul>
-      </div> */}
+
       {children}
     </div>
   );
