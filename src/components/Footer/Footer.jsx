296a5106c4907a27851fb52e1095bc34a7352782
Robert Cerdas <rocerdasgo@gmail.com>
2025-06-30 14:41:06 -0600

    chore: update dependencies

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,62 +2,62 @@ import React from 'react';
 import './Footer.scss';
 
 const codeWarsIcon =
-  'https://raw.githubusercontent.com/codewars/branding/refs/heads/master/logo.png';
+    'https://raw.githubusercontent.com/codewars/branding/refs/heads/master/logo.png';
 
 const Footer = () => {
-  return (
-    <footer>
-      <div className='footer-container'>
-        <h3>Socials</h3>
-        <div className='footer-content'>
-          <div className='footer-socials-container'>
-            <ul>
-              <li>
-                <a
-                  href='https://www.linkedin.com/in/robertcego/'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img
-                    src='https://cdn-icons-png.flaticon.com/512/174/174857.png'
-                    alt='LinkedIn'
-                    style={{ width: '48px', height: '48px' }}
-                  />
-                </a>
-              </li>
-              <li>
-                <a
-                  href='https://www.github.com/Robertcego'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img
-                    src='https://cdn-icons-png.flaticon.com/512/25/25231.png'
-                    alt='Github'
-                    style={{ width: '48px', height: '48px' }}
-                  />
-                </a>
-              </li>
-              <li>
-                <a
-                  href='https://www.codewars.com/users/Robertcego'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <img
-                    src={codeWarsIcon}
-                    alt='Codewars'
-                    title='Codewars'
-                    style={{ width: '48px', height: '48px' }}
-                  />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+    return (
+        <footer>
+            <div className='footer-container'>
+                <h3>Socials</h3>
+                <div className='footer-content'>
+                    <div className='footer-socials-container'>
+                        <ul>
+                            <li>
+                                <a
+                                    href='https://www.linkedin.com/in/robertcego/'
+                                    target='_blank'
+                                    rel='noopener noreferrer'
+                                >
+                                    <img
+                                        src='https://cdn-icons-png.flaticon.com/512/174/174857.png'
+                                        alt='LinkedIn'
+                                        style={{ width: '48px', height: '48px' }}
+                                    />
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href='https://www.github.com/Robertcego'
+                                    target='_blank'
+                                    rel='noopener noreferrer'
+                                >
+                                    <img
+                                        src='https://cdn-icons-png.flaticon.com/512/25/25231.png'
+                                        alt='Github'
+                                        style={{ width: '48px', height: '48px' }}
+                                    />
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href='https://www.codewars.com/users/Robertcego'
+                                    target='_blank'
+                                    rel='noopener noreferrer'
+                                >
+                                    <img
+                                        src={codeWarsIcon}
+                                        alt='Codewars'
+                                        title='Codewars'
+                                        style={{ width: '48px', height: '48px' }}
+                                    />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
